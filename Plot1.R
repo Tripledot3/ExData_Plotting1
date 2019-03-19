3bf4b0144c5354aeac69a01df912de53a8e9c170
Davon <davon.dicks@gmail.com>
2019-03-19 15:07:34 -0400

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,5 +1,5 @@
 EPC <- read.table("Plots/household_power_consumption.txt", header =  TRUE, sep = ";",stringsAsFactors = FALSE, na.strings = "?")
-# loading the data into R
+# loading the dataset into R
 
 EPC_NA <- subset(EPC, Date == "1/2/2007" | Date == "2/2/2007")
 #Subetting the days that we want to use
@@ -12,7 +12,10 @@ EPC$Global_active_power <- as.numeric(EPC$Global_active_power)
 #changing the Global active power colunm in the data from the character class to the number class
 
 hist(EPC_NA$Global_active_power, col = "red", xlab ="Global Active Power (kilowatts)", main = "Global Active Power")
+#ploting the histogram
 
 dev.copy(png, file = "plot1.png")
+#copying the plot to a png file
 
 dev.off()
+#closing the png file
