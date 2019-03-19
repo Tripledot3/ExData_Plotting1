40941eacfc08a7621346f334d71a4b75c145a41f
Davon <davon.dicks@gmail.com>
2019-03-19 15:10:53 -0400

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -8,15 +8,18 @@ datetime <- strptime(paste(EPC_plot$Date, EPC_plot$Time, sep=" "), "%d/%m/%Y %H:
 #defintely the answer
 
 EPC_plot$Global_active_power <- as.numeric(EPC_plot$Global_active_power)
-#changing the Global active power colunm in the data from the
-#character class to the number class
+#changing the Global active power colunm in the data from the character class to the number class
 
 plot(datetime, EPC_plot$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
 lines(datetime, EPC_plot$Sub_metering_2, type = "l", col = "red")
 lines(datetime, EPC_plot$Sub_metering_3, type = "l", col = "blue")
+# creating a plot with two other line points
 
 legend("topright", lty=1, lwd=2.5, col = c("black","blue","red"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+# creating a legend that displays information in the plotted graph
 
 dev.copy(png, file = "plot3.png")
+#copying the plot to a png file
 
 dev.off()
+#closing the png file
