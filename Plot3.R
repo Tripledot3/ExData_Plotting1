f2329fa8d74568a4f62ff7976225bfabd6d1de30
Davon <davon.dicks@gmail.com>
2019-03-19 15:20:13 -0400

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -5,7 +5,7 @@ EPC_plot <- subset(EPC, Date == "1/2/2007" | Date == "2/2/2007")
 #Subetting the days that we want to use
 
 datetime <- strptime(paste(EPC_plot$Date, EPC_plot$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-#defintely the answer
+#converting the dataframe to a date in the format that we want to use
 
 EPC_plot$Global_active_power <- as.numeric(EPC_plot$Global_active_power)
 #changing the Global active power colunm in the data from the character class to the number class
