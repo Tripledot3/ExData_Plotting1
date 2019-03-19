b3a5d178e3254ee01e37af2a9a396b8e93c35719
Davon <davon.dicks@gmail.com>
2019-03-19 15:17:03 -0400

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -12,16 +12,24 @@ EPC_plot$Global_active_power <- as.numeric(EPC_plot$Global_active_power)
 #character class to the number class
 
 par(mfrow = c(2,2))
-# setting the layout
+# setting the layout/position that the plots will be in
 
 plot(datetime, EPC_plot$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
-
+#ploting the first plot
 plot(datetime, EPC_plot$Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
-
+#ploting the secound plot
 
 plot(datetime, EPC_plot$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
 lines(datetime, EPC_plot$Sub_metering_2, type = "l", col = "red")
 lines(datetime, EPC_plot$Sub_metering_3, type = "l", col = "blue")
 legend("topright", lty=1, bty = "n", col = c("black","blue","red"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+#ploting the third plot with the 2 other points and a legend
 
 plot(datetime, EPC_plot$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power")
+#plotting the fourth plot
+
+dev.copy(png, file = "plot4.png")
+#copying the plot to a png file
+
+dev.off()
+#closing the png file
